d7960638642303b4a33fbb2e7557020cb54d91b2
yellowpeanut <100905123+yellowpeanut@users.noreply.github.com>
2023-03-15 11:17:30 +0300

    Add files via upload

--- 2.py
+++ 2.py
@@ -100,7 +100,7 @@ M3 = find_M(m_ins[2], _m_sv)
 _C_out = C_outs[2]
 t = ts[0]
 while t <= ts[1]:
-    _C_out, M2 = f(C_ins[2], _C_out, m_ins[2], M2, _Tv, _dt)
+    _C_out, M3 = f(C_ins[2], _C_out, m_ins[2], M3, _Tv, _dt)
     Cs3.append(_C_out)
     t += _dt
 
@@ -117,6 +117,7 @@ while t <= ts[1]+100:
     tt.append(t)
     t += _dt
 
+C1, C2, C3 = [], [], []
 CCs1, CCs2, CCs3 = [], [], []
 for i in range(1000):
     Cs1.append(Cs1[len(Cs1)-1])
@@ -125,18 +126,19 @@ for i in range(1000):
     CCs1.append(C_outs[0])
     CCs2.append(C_outs[1])
     CCs3.append(C_outs[2])
+    C1.append(C_ins[0])
+    C2.append(m_ins[1])
+    C3.append(Tvs[2])
 Cs1 = CCs1 + Cs1
 Cs2 = CCs2 + Cs2
 Cs3 = CCs3 + Cs3
 
-C1, C2, C3 = [], [], []
+
 for i in range(6000):
-    C1.append(C_outs2[0])
-    C2.append(C_outs2[1])
-    C3.append(C_outs2[2])
-C1 = CCs1 + C1
-C2 = CCs2 + C2
-C3 = CCs3 + C3
+    C1.append(C_ins[0]+dC_in)
+    C2.append(m_ins[1]+dm_in)
+    C3.append(Tvs[2]+dTv)
+
 
 
 fig, axis = plt.subplots(2, 3)
@@ -144,26 +146,26 @@ fig.tight_layout()
 
 axis[0][0].plot(tt, Cs1)
 axis[0][0].set_title("C_in to C_out")
-axis[0][0].set_xlabel("C_in")
+axis[0][0].set_xlabel("t")
 axis[0][0].set_ylabel("C_out")
 axis[1][0].plot(tt, C1)
-axis[1][0].set_xlabel("C_in")
-axis[1][0].set_ylabel("C_out")
+axis[1][0].set_xlabel("t")
+axis[1][0].set_ylabel("C_in")
 
 axis[0][1].plot(tt, Cs2)
 axis[0][1].set_title("m_in to C_out")
-axis[0][1].set_xlabel("m_in")
+axis[0][1].set_xlabel("t")
 axis[0][1].set_ylabel("C_out")
 axis[1][1].plot(tt, C2)
-axis[1][1].set_xlabel("m_in")
-axis[1][1].set_ylabel("C_out")
+axis[1][1].set_xlabel("t")
+axis[1][1].set_ylabel("m_in")
 
 axis[0][2].plot(tt, Cs3)
 axis[0][2].set_title("Tv_in to C_out")
-axis[0][2].set_xlabel("Tv_in")
+axis[0][2].set_xlabel("t")
 axis[0][2].set_ylabel("C_out")
 axis[1][2].plot(tt, C3)
-axis[1][2].set_xlabel("Tv_in")
-axis[1][2].set_ylabel("C_out")
+axis[1][2].set_xlabel("t")
+axis[1][2].set_ylabel("Tv")
 
 plt.show()
